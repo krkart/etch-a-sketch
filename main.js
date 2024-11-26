6b93cfbf2b61aa5633b05e501180fb3a8e720ed5
krkart <krkart@zohomail.in>
2024-11-26 10:04:51 +0530

    squares per board logic in progress

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let parent = document.querySelector('.parent');
+let board = document.querySelector('.board');
 let input = document.querySelector('#squares');
 let btn = document.querySelector('#submit-btn');
 
@@ -6,9 +6,16 @@ btn.addEventListener('click', () => {
   let userInput = parseInt(input.value);
   console.log(userInput);
   for (let i = 1; i <= userInput; i++) {
-    let childDiv = document.createElement('div');
-    childDiv.className = 'child';
-    childDiv.textContent = `${i}`;
-    parent.appendChild(childDiv);
+    let col = document.createElement('div')
+    col.id = `c${i}`;
+    col.className = 'col';
+    board.appendChild(col);
+    for (let j = 1; j <= userInput; j++) {
+      let row = document.createElement('div');
+      row.id = `c${i}r${j}`;
+      row.className = 'block';
+      row.style.flexBasis = `${100/userInput}%`;
+      col.appendChild(row);
+    }
   }
 })
