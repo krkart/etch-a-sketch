5ae4cd04c6114a71da9a6e9f338fd581354e4259
krkart <krkart@zohomail.in>
2024-11-26 21:50:11 +0530

    clear button and clear board logic complete

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let board = document.querySelector('.board');
 let slider = document.querySelector('input[type="range"]');
 let sliderValue = document.querySelector('.slider-value');
+let clearBtn = document.querySelector('.clear-btn');
+clearBtn.addEventListener('click', clearAll);
+
+clearAll();
 
 slider.addEventListener('mouseup', () => {
+  clearBoard();
   let userInput = parseInt(slider.value);
   sliderValue.textContent = `${userInput}`;
   for (let i = 1; i <= userInput; i++) {
@@ -19,3 +24,26 @@ slider.addEventListener('mouseup', () => {
     }
   }
 })
+
+function clearAll() {
+  clearBoard();
+  sliderValue.textContent = '16';
+  slider.value = '16';
+  for (let i = 1; i <= slider.value; i++) {
+    let col = document.createElement('div')
+    col.id = `c${i}`;
+    col.className = 'col';
+    board.appendChild(col);
+    for (let j = 1; j <= slider.value; j++) {
+      let row = document.createElement('div');
+      row.id = `c${i}r${j}`;
+      row.className = 'block';
+      row.style.flexBasis = `${100/slider.value}%`;
+      col.appendChild(row);
+    }
+  }
+}
+
+function clearBoard() {
+  Array.from(board.children).forEach(child => child.remove());
+}
