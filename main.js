656d7409a407ef2350dba53cbc2544d119a88aea
krkart <krkart@zohomail.in>
2024-11-26 22:50:49 +0530

    divs aligned in perfect

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ clearAll();
 slider.addEventListener('mouseup', () => {
   clearBoard();
   let userInput = parseInt(slider.value);
-  sliderValue.textContent = `${userInput}`;
+  sliderValue.textContent = userInput;
   for (let i = 1; i <= userInput; i++) {
     let col = document.createElement('div')
     col.id = `c${i}`;
@@ -28,7 +28,7 @@ slider.addEventListener('mouseup', () => {
 function clearAll() {
   clearBoard();
   sliderValue.textContent = '16';
-  slider.value = '16';
+  slider.value = 16;
   for (let i = 1; i <= slider.value; i++) {
     let col = document.createElement('div')
     col.id = `c${i}`;
@@ -45,5 +45,7 @@ function clearAll() {
 }
 
 function clearBoard() {
-  Array.from(board.children).forEach(child => child.remove());
+  // Ref: https://stackoverflow.com/a/74886266/6028958
+  // Array.from(board.children).forEach(child => child.remove());
+  board.replaceChildren();
 }
