47065cead64f9768c9e07993d6c535259fce7090
krkart <krkart@zohomail.in>
2024-11-27 08:36:16 +0530

    divs alignment in progress

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let board = document.querySelector('.board');
 let slider = document.querySelector('input[type="range"]');
 let sliderValue = document.querySelector('.slider-value');
-let clearBtn = document.querySelector('.clear-btn');
-clearBtn.addEventListener('click', clearAll);
+let resetBtn = document.querySelector('#reset-btn');
+resetBtn.addEventListener('click', reset);
 
-clearAll();
+reset();
 
 slider.addEventListener('mouseup', () => {
-  clearBoard();
+  board.replaceChildren();
   let userInput = parseInt(slider.value);
   sliderValue.textContent = userInput;
   for (let i = 1; i <= userInput; i++) {
@@ -25,8 +25,8 @@ slider.addEventListener('mouseup', () => {
   }
 })
 
-function clearAll() {
-  clearBoard();
+function reset() {
+  board.replaceChildren();
   sliderValue.textContent = '16';
   slider.value = 16;
   for (let i = 1; i <= slider.value; i++) {
@@ -44,8 +44,10 @@ function clearAll() {
   }
 }
 
+/*
 function clearBoard() {
   // Ref: https://stackoverflow.com/a/74886266/6028958
   // Array.from(board.children).forEach(child => child.remove());
   board.replaceChildren();
 }
+*/
